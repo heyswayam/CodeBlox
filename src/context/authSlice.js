f0986145d21db0e9d89d238946b9c55136ecc240
SwayamMac <heyyswayam@gmail.com>
2024-08-11 22:03:16 +0530

    bug: removed localStorage from authSlice to fix the logout error
    - It was showing logged in because of localStorage due to which log out didn't [ Logout error: User (role: guests) missing scope (account) ]

--- src/context/authSlice.js
+++ src/context/authSlice.js
@@ -2,7 +2,6 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
     status: JSON.parse(localStorage.getItem('authStatus')) || false,
-    userData: JSON.parse(localStorage.getItem('userData')) || null
 };
 
 const authSlice = createSlice({
@@ -12,14 +11,12 @@ const authSlice = createSlice({
         login: (state, action) => {
             state.status = true;
             state.userData = action.payload;
-            localStorage.setItem('authStatus', JSON.stringify(true));
-            localStorage.setItem('userData', JSON.stringify(action.payload));
+
         },
         logout: (state) => {
             state.status = false;
             state.userData = null;
-            localStorage.removeItem('authStatus');
-            localStorage.removeItem('userData');
+
         }
     }
 });
