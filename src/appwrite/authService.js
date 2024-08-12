94612563cc0f5b5b5d74d7c45e92dac2589f44da
SwayamMac <heyyswayam@gmail.com>
2024-08-12 13:38:35 +0530

    fix: MAJOR update: added signUp and singIn functions inside authService.js which has been deleted by mistake

--- src/appwrite/authService.js
+++ src/appwrite/authService.js
@@ -1,67 +1,83 @@
 import { Client, Account, ID } from "appwrite";
 import conf_env from "../conf_env/conf_env";
 
-
 class AuthService {
-    constructor() {
-        this.client = new Client().setEndpoint(conf_env.appwriteUrl).setProject(conf_env.projectId);
-        this.account = new Account(this.client);
-    }
+	constructor() {
+		this.client = new Client().setEndpoint(conf_env.appwriteUrl).setProject(conf_env.projectId);
+		this.account = new Account(this.client);
+	}
+	async signUp({ email, password, name }) {
+		try {
+			const newAccount = await this.account.create(ID.unique(), email, password, name);
+			return newAccount;
+		} catch (error) {
+			console.log("auth.js :: signUp :: error " + error);
+			throw error;
+		}
+	}
 
-    async logout() {
-        try {
-            return await this.account.deleteSessions("current");
-        } catch (error) {
-            console.log("auth.js :: logout :: error " + error);
-            throw error;
-        }
-    }
+	async signIn({ email, password }) {
+		try {
+			const newAccount = await this.account.createEmailPasswordSession(email, password);
+			return newAccount;
+		} catch (error) {
+			console.log("auth.js :: signIn :: error " + error);
+			throw error;
+		}
+	}
+	async logout() {
+		try {
+			return await this.account.deleteSessions("current");
+		} catch (error) {
+			console.log("auth.js :: logout :: error " + error);
+			throw error;
+		}
+	}
 
-    async getCurrUser() {
-        try {
-            return await this.account.get();
-        } catch (error) {
-            console.log("auth.js :: getCurrUser :: error " + error);
-            throw error;
-        }
-    }
+	async getCurrUser() {
+		try {
+			return await this.account.get();
+		} catch (error) {
+			console.log("auth.js :: getCurrUser :: error " + error);
+			throw error;
+		}
+	}
 
-    async passwordLessSignin(email) {
-        try {
-            const result = await this.account.createMagicURLToken(
+	async passwordLessSignin(email) {
+		try {
+			const result = await this.account.createMagicURLToken(
 				ID.unique(), // userId
 				email, // email
-                // "http://localhost:5173/redirecting" // url (optional)
-                "https://codebloxapp.vercel.app/redirecting" // url (optional)
+				// "http://localhost:5173/redirecting" // url (optional)
+				"https://codebloxapp.vercel.app/redirecting", // url (optional)
 				// false // phrase (optional) // commenting this only works
 			);
-            // console.log("Magic URL session created:", result);
-        } catch (error) {
-            console.log("auth.js :: passwordLessSignin :: error " + error);
-            throw error;
-        }
-    }
+			// console.log("Magic URL session created:", result);
+		} catch (error) {
+			console.log("auth.js :: passwordLessSignin :: error " + error);
+			throw error;
+		}
+	}
 
-	async getPasswordlesstoken(){
+	async getPasswordlesstoken() {
 		try {
 			const urlParams = new URLSearchParams(window.location.search);
 			console.log("urlParams: " + urlParams);
-			
+
 			const userId = urlParams.get("userId");
 			const secret = urlParams.get("secret");
-            
+
 			// if (userId && secret) {
-				const session = await this.account.createSession(userId, secret);
-				// console.log("Session updated:", session);
-				return session;
+			const session = await this.account.createSession(userId, secret);
+			// console.log("Session updated:", session);
+			return session;
 			// } else {
 			// 	throw new Error("userId or secret not found in URL parameters");
 			// }
 		} catch (error) {
 			console.log("auth.js :: getPasswordlesstoken :: error " + error);
-            throw error;
+			throw error;
 		}
-
 	}
 }
 
