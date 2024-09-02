713975beeb89286d2e3b3894b1c1e3fe52265af9
SwayamMac <heyyswayam@gmail.com>
2024-09-02 21:07:21 +0530

    removed stringifying

--- src/conf_env/conf_env.js
+++ src/conf_env/conf_env.js
@@ -5,7 +5,7 @@ const conf_env = {
     colectionId: String(import.meta.env.VITE_COLLECTION_ID),
     bucketId: String(import.meta.env.VITE_BUCKET_ID),
     tinyMCEId: String(import.meta.env.VITE_TINYMCE_ID),
-    geminiApi: String(import.meta.env.VITE_GENAI_ID)
+    geminiApi: import.meta.env.VITE_GENAI_ID
 }
 
 export default conf_env;
